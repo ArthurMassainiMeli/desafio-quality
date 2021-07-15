62596df0669582b417ff0ec51eca4872269b0292
CaioMF <caio.fagundes@mercadolivre.com>
2021-07-15 17:42:50 -0300

    Config project DB

---
+++ src/test/java/com/meli/br/imoveis/ImoveisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meli.br.imoveis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImoveisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
