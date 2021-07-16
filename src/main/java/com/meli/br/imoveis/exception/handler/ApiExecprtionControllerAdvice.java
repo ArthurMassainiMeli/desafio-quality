5369b99e657ce50940a45d59a699399acf644d64
Arthur Massaini <arthur.massaini@mercadolivre.com>
2021-07-16 11:04:10 -0300

    handlers exception

--- src/main/java/com/meli/br/imoveis/exception/handler/ApiExecprtionControllerAdvice.java
+++ src/main/java/com/meli/br/imoveis/exception/handler/ApiExecprtionControllerAdvice.java
@@ -1,13 +1,19 @@
 package com.meli.br.imoveis.exception.handler;
 
+import com.meli.br.imoveis.dto.ExceptionDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ApiExecprtionControllerAdvice {
@@ -28,4 +34,26 @@ public class ApiExecprtionControllerAdvice {
         return ResponseEntity.badRequest().body(errors);
     }
 
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<ExceptionDTO> defaultHandler(NoSuchElementException e) {
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .body(new ExceptionDTO(e.getMessage(), HttpStatus.NOT_FOUND.value()));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ResponseEntity<ExceptionDTO> defaultHandler(HttpMessageNotReadableException e) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ExceptionDTO("Requisição mal formatada.", HttpStatus.BAD_REQUEST.value()));
+    }
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ExceptionDTO> defaultHandler(HttpRequestMethodNotSupportedException e) {
+
+        String msg = e.getMethod() + " não suportado para essa rota!";
+
+        return ResponseEntity.status(HttpStatus.METHOD_NOT_ALLOWED.value()).body(new ExceptionDTO(msg, HttpStatus.METHOD_NOT_ALLOWED.value()));
+    }
 }
