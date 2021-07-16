8b21b6a5b08bbe222385a3da019e9bb4124f50c0
iventorim <igor.ventorim@gmail.com>
2021-07-16 15:37:40 -0300

    Removendo repositories não utilizados

--- src/main/java/com/meli/br/imoveis/repository/PropertyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meli.br.imoveis.repository;
-
-import com.meli.br.imoveis.entity.Property;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PropertyRepository extends JpaRepository<Property, Long> {
-}
--- src/main/java/com/meli/br/imoveis/service/RoomService.java
+++ src/main/java/com/meli/br/imoveis/service/RoomService.java
@@ -2,8 +2,6 @@ package com.meli.br.imoveis.service;
 
 import com.meli.br.imoveis.dto.RoomTotalDTO;
 import com.meli.br.imoveis.entity.Room;
-import com.meli.br.imoveis.repository.RoomRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
@@ -12,13 +10,6 @@ import java.util.stream.Collectors;
 
 @Service
 public class RoomService {
-
-    private final RoomRepository roomRepository;
-
-    @Autowired
-    public RoomService(RoomRepository roomRepository) {
-        this.roomRepository = roomRepository;
-    }
 
     public double calcArea(Room r) {
         return r.getRoomWidth() * r.getRoomLength();
